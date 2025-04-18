b9b5719cd2be2c307c03bff44bd235f160b10d0b
Musa <musasayar67@gmail.com>
2025-04-18 18:04:18 +0300

    added login functionality

--- services/auth/authService.ts
+++ services/auth/authService.ts
@@ -9,13 +9,36 @@ interface RegisterData {
   password: string;
 }
 
+interface LoginData {
+  email: string;
+  password: string;
+}
+
 export const register = createAsyncThunk(
   "user/register",
   async (data: RegisterData, { rejectWithValue }) => {
-  console.log("data", data)
+    console.log("data", data);
     try {
       const response = await api.post("/api/v1/auth/register", data);
       console.log("response", response.data);
+      return response.data;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        const message =
+          error.response?.data?.message || "Bilinmeyen bir hata oluştu.";
+        return rejectWithValue(message);
+      } else {
+        return rejectWithValue("Bilinmeyen bir hata oluştu.");
+      }
+    }
+  }
+);
+
+export const login = createAsyncThunk(
+  "user/login",
+  async (data, { rejectWithValue }) => {
+    try {
+      const response = await api.post("/api/v1/auth/login", data);
       return response.data;
     } catch (error) {
       if (axios.isAxiosError(error)) {
